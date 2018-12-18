61c0453b509b4bef61f350143c7074aad03ddcef
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-12-18 17:57:31 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,8 +1,8 @@
 class PigLatinizer
-  attr_reader :text
+  attr_reader :words
 
-  def initialize(text)
-    @text = text.downcase
+  def initialize(words)
+    @words = words.downcase
   end
 
 end
